79fc02179f4667632b174e078cd9572f4ecab6bb
YahirCC <147668023+VEGCRh@users.noreply.github.com>
2025-10-16 10:29:55 -0600

    feature/icon

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -1,11 +1,14 @@
 self.addEventListener("install", (event) => {
   console.log("Service Worker instalado");
   event.waitUntil(
-    caches.open("worklink-cache").then((cache) => {
+    caches.open("worklink-cache-v2").then((cache) => {
       return cache.addAll([
         "/", 
         "/static/css/style_index.css",
         "/static/css/style_login.css",
+        "/static/icons/icon-192x192.png",
+        "/static/icons/icon-512x512.png",
+        "/static/manifest.json"
       ]);
     })
   );
